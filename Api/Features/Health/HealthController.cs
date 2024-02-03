a63dcd7c6c69d125be0d0b3c6f8b233c54cd93c1
Jason <jasonhew1211@gmail.com>
2024-02-03 22:19:07 +0800

    Add health controller

---
+++ Api/Features/Health/HealthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MediatR;
+using Api.Seedwork;
+using System.Net;
+
+namespace Api.Features.Health
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IMediator _mediator;
+
+        public HealthController(
+            ILogger<HealthController> logger,
+            IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Accepted)]
+        public async Task<IActionResult> Health()
+        {
+            return Ok("Ok");
+        }
+    }
+}
